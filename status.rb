f512604656f20f1fd8348d1cf16221b0e8ff4274
Arfon Smith <arfon@github.com>
2014-12-09 21:39:12 -0600

    URL debugging

--- status.rb
+++ status.rb
@@ -3,7 +3,7 @@ require 'octokit'
 @client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
 
 if pull_request_id = ENV['TRAVIS_PULL_REQUEST']
-  `travis-artifacts upload --path assets/result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png`
+  `travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
   image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
   @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: ![](#{image_url})")
 end
