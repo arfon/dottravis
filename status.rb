15d06a31b13ad0c3fc3f3af69f67e3ef0a2a85ef
Arfon Smith <arfon@github.com>
2014-12-09 21:08:29 -0600

    Comment on the PR not the commit

--- status.rb
+++ status.rb
@@ -3,5 +3,5 @@ require 'octokit'
 @client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
 
 if pull_request_id = ENV['TRAVIS_PULL_REQUEST']
-  @client.create_commit_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_COMMIT'], ":shipit:")
+  @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit:")
 end
