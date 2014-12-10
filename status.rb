4c810ec373d68057accfd0d1dd6b1735414ffea2
Andy Casey <andycasey@gmail.com>
2014-12-09 22:06:03 -0600

    Create a scoreboard

--- status.rb
+++ status.rb
@@ -6,4 +6,20 @@ if pull_request_id = ENV['TRAVIS_PULL_REQUEST']
   `travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
   image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
   @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: ![](#{image_url})")
+
+  ` Check to see if the file we want exists`
+  scoreboard_path = 'scoreboard.csv'
+  all_paths = @client.contents(ENV['TRAVIS_REPO_SLUG'])
+  if not all_paths.map(&:path).include? scoreboard_path
+
+    ` OK, let's create a scoreboard file`
+    parameters = ENV.grep(/^RESULT_/)
+    scoreboard_contents = '# ' + parameters.join(', ') + '\n'
+    scoreboard_contents += ENV['TRAVIS_BRANCH']
+    scoreboard_contents += parameters.map { |x| ENV[x] }.join(',') + '\n'
+
+    @client.create_contents(ENV['TRAVIS_REPO_SLUG'],
+      scoreboard_path,
+      "Created the scoreboard",
+      scoreboard_contents)
 end
