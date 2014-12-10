3583ef59afcccf3c45a79e6df87a86bf4c582b9f
Andy Casey <andycasey@gmail.com>
2014-12-10 02:19:00 -0600

    y

--- status.rb
+++ status.rb
@@ -3,6 +3,21 @@ require 'yaml'
 
 @client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
 
+
+if pull_request_id = ENV['TRAVIS_PULL_REQUEST']
+  `travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
+  image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
+  @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: ![](#{image_url})")
+end
+
+
+
+
+
+
+
+
+
 puts("check")
 #puts(ENV['TRAVIS_PULL_REQUEST'])
 #puts("#{ENV['TRAVIS_PULL_REQUEST']}")
@@ -16,10 +31,10 @@ puts("check")
 
 #if ENV['TRAVIS_PULL_REQUEST'] != 'false'
 
-`travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
-image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
+#{}`travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
+#image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
 
-@client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: what up ![](#{image_url})")
+#@client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: what up ![](#{image_url})")
 
 puts("added")
 result_path = 'result.csv'
