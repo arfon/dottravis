6cc68fcc94f76a4ad1694476f318d9ff92c82d9b
Andy Casey <andycasey@gmail.com>
2014-12-10 00:54:54 -0600

    ping

--- status.rb
+++ status.rb
@@ -5,13 +5,15 @@ require 'yaml'
 
 puts("check")
 puts(ENV['TRAVIS_PULL_REQUEST'])
+puts(ENV['TRAVIS_PULL_REQUEST'] != 'false')
 puts(ENV['TRAVIS_PULL_REQUEST'] != "false")
 puts(ENV['TRAVIS_PULL_REQUEST'] == false)
-if ENV['TRAVIS_PULL_REQUEST'] != "false"
+
+if ENV['TRAVIS_PULL_REQUEST'] != 'false'
   `travis-artifacts upload --path assets/result.png:result.png --target-path results/#{ENV['TRAVIS_PULL_REQUEST']}/`
   image_url = "https://s3.amazonaws.com/#{ENV['ARTIFACTS_S3_BUCKET']}/results/#{ENV['TRAVIS_PULL_REQUEST']}/result.png"
   
-  @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: +1 ![](#{image_url})")
+  @client.add_comment(ENV['TRAVIS_REPO_SLUG'], ENV['TRAVIS_PULL_REQUEST'], ":shipit: ![](#{image_url})")
 
   puts("added")
   result_path = 'result.csv'
